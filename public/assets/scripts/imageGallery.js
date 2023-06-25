dd0fa028cf1b8f63ee8cd07935d46e164aaf8bfa
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2023-06-25 04:25:55 -0300

    Reintoducing sass repository.

--- public/assets/scripts/imageGallery.js
+++ public/assets/scripts/imageGallery.js
@@ -3,23 +3,3 @@ let imageGalleryImgs = document.querySelectorAll('#imageGallery img')
 imageGalleryImgs.forEach(item => {
     console.log(item)
 })
-
-
-if(screen.width <= 764){
-    imageGalleryImgs.forEach((item, index) => {
-        switch (index){
-            case 0:
-                item.src = '../../images/mobile/image-gallery-milkbottles.jpg'
-            break
-            case 1:
-                item.src = '../../images/mobile/image-gallery-orange.jpg'
-                break
-            case 2:
-                item.src = '../../images/mobile/image-gallery-cone.jpg'
-                break
-            case 3:
-                item.src = '../../images/mobile/image-gallery-sugar-cubes.jpg'
-                break
-        }
-    })
-}
